df1d46aabde169ae87c279d8f4d1844b5c514a26
Dan Johnson <danielsjohnson@meta.com>
2024-03-26 14:25:40 -0700

    ctran all to all copy16 unit test
    Summary: Adding a unit test where I found the ctran all to all fail, while the orig implementation succeeds. See D55000136 for the fix and for more information.
    
    Reviewed By: minsii
    
    Differential Revision: D54973652
    
    fbshipit-source-id: 97913250c86b865d9c03702d33ba1e1ead71744e

--- src/tests/AllToAllvTest.cc
+++ src/tests/AllToAllvTest.cc
@@ -44,6 +44,9 @@ class AllToAllvTest : public ::testing::Test {
 
   template <typename T>
   void assignChunkValue(T* buf, size_t count, T val) {
+    if(count == 0){
+      return;
+    }
     std::vector<T> expectedVals(count, val);
     CUDACHECKIGNORE(cudaMemcpy(
         buf, expectedVals.data(), count * sizeof(T), cudaMemcpyDefault));
@@ -71,6 +74,129 @@ class AllToAllvTest : public ::testing::Test {
     }
     return errs;
   }
+
+  void runCanCopy16Mismatch(bool registFlag = false) {
+#ifdef NCCL_ALLTOALLV_SUPPORTED
+    if(this->numRanks < 4){
+      std::cout << "Need at least four ranks to run this test." << std::endl;
+      return;
+    }
+
+    if(this->globalRank > 3){
+      return;
+    }
+
+    // prepare alltoallv arguments
+    std::vector<size_t> sendCounts(this->numRanks);
+    std::vector<size_t> sendDispls(this->numRanks);
+    std::vector<size_t> recvCounts(this->numRanks);
+    std::vector<size_t> recvDispls(this->numRanks);
+    if(this->globalRank == 0){
+      sendCounts[0] = 20000775;
+      sendCounts[1] = 20000171;
+      sendCounts[2] = 20000806;
+      sendCounts[3] = 20000365;
+      recvCounts[0] = 20000775;
+      recvCounts[1] = 20000316;
+      recvCounts[2] = 20000575;
+      recvCounts[3] = 20000954;
+    }
+    else if(this->globalRank == 1){
+      sendCounts[0] = 20000316;
+      sendCounts[1] = 20000564;
+      sendCounts[2] = 20000432;
+      sendCounts[3] = 20000625;
+      recvCounts[0] = 20000171;
+      recvCounts[1] = 20000564;
+      recvCounts[2] = 20000529;
+      recvCounts[3] = 20000582;
+    }
+    else if(this->globalRank == 2){
+      sendCounts[0] = 20000575;
+      sendCounts[1] = 20000529;
+      sendCounts[2] = 20000343;
+      sendCounts[3] = 20000841;
+      recvCounts[0] = 20000806;
+      recvCounts[1] = 20000432;
+      recvCounts[2] = 20000343;
+      recvCounts[3] = 20000763;
+    }
+    else if(this->globalRank == 3){
+      sendCounts[0] = 20000954;
+      sendCounts[1] = 20000582;
+      sendCounts[2] = 20000763;
+      sendCounts[3] = 20000142;
+      recvCounts[0] = 20000365;
+      recvCounts[1] = 20000625;
+      recvCounts[2] = 20000841;
+      recvCounts[3] = 20000142;
+    }
+    sendDispls[0] = 0;
+    recvDispls[0] = 0;
+    for (int i = 1; i < 4; i++) {
+      sendDispls[i] = sendDispls[i-1] + sendCounts[i-1];
+      recvDispls[i] = recvDispls[i-1] + recvCounts[i-1];
+    }
+
+    int sendCount = 0;
+    int recvCount = 0;
+    for (int i = 0; i < 4; i++) {
+      sendCount += sendCounts[i];
+      recvCount += recvCounts[i];
+    }
+
+    // create and register buffers
+    int *sendBuf = nullptr, *recvBuf = nullptr;
+    void *sendHandle = nullptr, *recvHandle = nullptr;
+
+    CUDACHECK_TEST(cudaMalloc(&sendBuf, sendCount * sizeof(int)));
+    CUDACHECK_TEST(cudaMalloc(&recvBuf, recvCount * sizeof(int)));
+
+    int expectedVal = 32;
+    assignChunkValue(sendBuf, sendCount, expectedVal);
+    assignChunkValue(recvBuf, recvCount, -1);
+
+    if (registFlag) {
+      NCCLCHECK_TEST(ncclCommRegister(
+          comm, sendBuf, sendCount * sizeof(int), &sendHandle));
+      NCCLCHECK_TEST(ncclCommRegister(
+          comm, recvBuf, recvCount * sizeof(int), &recvHandle));
+    }
+
+    // run alltoallv
+    auto res = ncclAllToAllv(
+        sendBuf,
+        sendCounts.data(),
+        sendDispls.data(),
+        recvBuf,
+        recvCounts.data(),
+        recvDispls.data(),
+        ncclInt,
+        comm,
+        stream);
+    ASSERT_EQ(res, ncclSuccess);
+    CUDACHECK_TEST(cudaStreamSynchronize(stream));
+
+
+    for (int r = 0; r < this->numRanks; r++) {
+      int expectedVal = 32;
+      int errs =
+          checkChunkValue(recvBuf + recvDispls[r], recvCounts[r], expectedVal);
+      EXPECT_EQ(errs, 0) << "rank " << this->globalRank << " checked chunk "
+                         << r << " at " << recvBuf + recvDispls[r] << " with "
+                         << errs << " errors";
+    }
+
+    if (registFlag) {
+      NCCLCHECK_TEST(ncclCommDeregister(comm, sendHandle));
+      NCCLCHECK_TEST(ncclCommDeregister(comm, recvHandle));
+    }
+
+    CUDACHECK_TEST(cudaFree(sendBuf));
+    CUDACHECK_TEST(cudaFree(recvBuf));
+#endif
+  }
+
 
   void run(bool registFlag = false) {
 #ifdef NCCL_ALLTOALLV_SUPPORTED
@@ -160,6 +286,19 @@ TEST_F(AllToAllvTest, Ctran) {
   unsetenv("NCCL_ALLTOALLV_ALGO");
 }
 
+TEST_F(AllToAllvTest, DISABLED_CtranCanCopy16Mismatch) {
+  setenv("NCCL_ALLTOALLV_ALGO", "ctran", 1);
+  ncclCvarInit();
+  runCanCopy16Mismatch();
+  unsetenv("NCCL_ALLTOALLV_ALGO");
+}
+
+TEST_F(AllToAllvTest, OrigCanCopy16Mismatch) {
+  setenv("NCCL_ALLTOALLV_ALGO", "orig", 1);
+  ncclCvarInit();
+  runCanCopy16Mismatch();
+  unsetenv("NCCL_ALLTOALLV_ALGO");
+}
 
 TEST_F(AllToAllvTest, InvalidSendbuf) {
 #ifdef NCCL_ALLTOALLV_SUPPORTED
